02d1f91dcc950b57b51629e2676294fcc02520c6
daklo91 <dennis.bjorneset@gmail.com>
2020-11-10 17:19:15 +0100

    Add pokemon image and sprite alternative to Browse

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
 # easydex
 
+api from pokeApi: https://pokeapi.co/
+img from official pokemon site: https://www.pokemon.com/us/pokedex/
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -3,10 +3,21 @@
     <button @click="$store.dispatch('fetchPokemonObject')">
       Fetch
     </button>
-    <!-- <p>{{ initialFetch.length }}</p> -->
+    <p>{{ pokemons.length }}</p>
     <div v-if="pokemons">
       <div v-for="pokemon in pokemons" :key="pokemon.id">
         <p>{{ pokemon.id + '# ' + pokemon.name }}</p>
+        <img
+          id="main-image"
+          :src="
+            'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' +
+              getPokemonId(pokemon.id) +
+              '.png'
+          "
+          :alt="'Image of ' + pokemon.name"
+          :title="'Image of ' + pokemon.name"
+          @error="getSprite(pokemon.id)"
+        />
       </div>
     </div>
   </div>
@@ -17,6 +28,19 @@ export default {
   computed: {
     pokemons() {
       return this.$store.getters.getSortedPokemonArray
+    }
+  },
+  methods: {
+    getSprite(id) {
+      event.target.src = this.pokemons[id - 1].sprites.front_default
+    },
+    getPokemonId(id) {
+      var idAsString = id.toString()
+      if (idAsString.length == 1) {
+        return '00' + idAsString
+      } else if (idAsString.length == 2) {
+        return '0' + idAsString
+      } else return idAsString
     }
   }
 }
