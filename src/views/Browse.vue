1e84ba3c5ed463a25c6bbe93602eab5bf47a84bf
daklo91 <dennis.bjorneset@gmail.com>
2020-11-11 16:53:38 +0100

    Add style to browse,  flex and align

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -4,10 +4,10 @@
       Fetch
     </button>
     <p>{{ pokemons.length }}</p>
-    <div v-if="pokemons">
+    <div v-if="pokemons" id="flex-container">
       <div v-for="pokemon in pokemons" :key="pokemon.id">
-        <div id="temp-frame">
-          <p>{{ pokemon.id + '# ' }}</p>
+        <div id="card">
+          <p id="id-number"><span id="id-hashtag">#</span>{{ pokemon.id }}</p>
           <img
             :src="pokemon.sprites.front_default"
             :alt="'Image of ' + pokemon.name"
@@ -31,9 +31,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#temp-frame {
-  border: solid $main-color 3px;
-  border-radius: 8px;
-  width: 250px;
+#flex-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  #card {
+    border: solid $main-color 3px;
+    border-radius: 8px;
+    width: 200px;
+    margin: 10px;
+    text-transform: capitalize;
+    background: radial-gradient(
+      circle,
+      rgb(206, 206, 206) 0%,
+      rgba(255, 255, 255, 1) 100%
+    );
+
+    #id-number {
+      text-align: left;
+      margin-left: 10px;
+
+      #id-hashtag {
+        font: 150% $sub-title-font;
+      }
+    }
+  }
 }
 </style>
