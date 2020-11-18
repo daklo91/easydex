49dbf61f730e510d508a377fa0a7513c2321eed1
daklo91 <dennis.bjorneset@gmail.com>
2020-11-18 14:06:31 +0100

    Made About.vue into a testing ground

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <hr />
+    <p>This is an about page.</p>
+    <p>But it can also be a test page.</p>
+    <hr />
+    <TypeIcon :type="'electric'" :size="200" />
+    <hr />
   </div>
 </template>
+
+<script>
+import TypeIcon from '@/components/TypeIcon.vue'
+
+export default {
+  components: {
+    TypeIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
