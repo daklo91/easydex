48cac294e3c171de6a7a184490a16ec9a6bb61a8
daklo91 <dennis.bjorneset@gmail.com>
2020-12-08 12:22:08 +0100

    Add card flip and some styling

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,52 +1,61 @@
 <template>
   <div>
     <div v-for="pokemon in pokemonArray" :key="pokemon.id">
-      <div v-if="Object.keys(pokemon).length > 1">
-        <div
-          id="card"
-          class="come-in"
-          :style="{
-            backgroundColor: setTypeColor(pokemon.types[0]) + 95,
-            borderColor: setTypeColor(
-              pokemon.types[1] ? pokemon.types[1] : pokemon.types[0]
-            )
-          }"
-        >
-          <div id="header">
-            <p id="id-number"><span id="id-hashtag">#</span>{{ pokemon.id }}</p>
-            <div id="type-icons">
-              <div
-                id="type-icon"
-                v-for="(types, index) in pokemon.types"
-                :key="index"
-              >
-                <TypeIcon :size="15" :type="pokemon.types[index]" />
+      <transition name="flip">
+        <div :key="10001" v-if="Object.keys(pokemon).length > 1">
+          <div
+            id="card"
+            class="come-in"
+            :style="{
+              backgroundColor: setTypeColor(pokemon.types[0]) + 95,
+              borderColor: setTypeColor(
+                pokemon.types[1] ? pokemon.types[1] : pokemon.types[0]
+              )
+            }"
+          >
+            <div id="header">
+              <p id="id-number">
+                <span id="id-hashtag">#</span>{{ pokemon.id }}
+              </p>
+              <div id="type-icons">
+                <div
+                  id="type-icon"
+                  v-for="(types, index) in pokemon.types"
+                  :key="index"
+                >
+                  <TypeIcon :size="15" :type="pokemon.types[index]" />
+                </div>
               </div>
             </div>
+            <img
+              id="image"
+              :src="pokemon.sprites.front_default"
+              :alt="'Image of ' + pokemon.name"
+              :title="'Image of ' + pokemon.name"
+            />
+            <p>{{ pokemon.species.name }}</p>
           </div>
-          <img
-            id="image"
-            :src="pokemon.sprites.front_default"
-            :alt="'Image of ' + pokemon.name"
-            :title="'Image of ' + pokemon.name"
-          />
-          <p>{{ pokemon.species.name }}</p>
         </div>
-      </div>
-      <div
-        v-else
-        id="empty-card"
-        v-observe-visibility="{
-          callback: (isVisible, entry) => {
-            pokemonInView(isVisible, pokemon)
-          },
-          once: true,
-          throttle: 700
-        }"
-      >
-        <p><span id="id-hashtag">#</span>{{ pokemon.id }}{{ pokemon.name }}</p>
-        Easydex
-      </div>
+        <div
+          :key="10002"
+          v-else
+          id="empty-card"
+          v-observe-visibility="{
+            callback: (isVisible, entry) => {
+              pokemonInView(isVisible, pokemon)
+            },
+            once: true,
+            throttle: 700
+          }"
+        >
+          <p>
+            <span id="id-hashtag">#</span>{{ pokemon.id }}{{ pokemon.name }}
+          </p>
+          <h3 class="easydex-title top">Easydex</h3>
+          <EasydexLogo :width="60" :height="70" />
+          <h3 class="easydex-title rotate">Easydex</h3>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -54,10 +63,12 @@
 <script>
 import typeColor from '@/scripts/typeColors.js'
 import TypeIcon from '@/components/TypeIcon.vue'
+import EasydexLogo from '@/assets/EasydexLogo.vue'
 
 export default {
   components: {
-    TypeIcon
+    TypeIcon,
+    EasydexLogo
   },
   props: {
     pokemonArray: {
@@ -81,15 +92,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.flip-enter-active {
+  transition: all 0.7s ease;
+}
+
+.flip-leave-active {
+  display: none;
+}
+
+.flip-enter,
+.flip-leave-to {
+  transform: rotateY(180deg);
+  opacity: 0;
+}
+
+.easydex-title {
+  font: 300% $title-font;
+  color: $pokemon-yellow-color;
+  -webkit-text-stroke-width: 1.7px;
+  -webkit-text-stroke-color: $pokemon-blue-color;
+  margin: 0px;
+}
+
+.rotate {
+  transform: rotate(-180deg);
+}
+
+.top {
+  margin-top: 20px;
+}
+
 #empty-card {
-  border: solid $main-color 2px;
+  border: solid rgb(117, 161, 255) 4px;
+  background: rgb(179, 199, 255);
   border-radius: 8px;
   width: 200px;
   height: 220px;
   margin: 10px;
   padding: 5px;
   box-sizing: border-box;
-
+  p {
+    text-align: left;
+    margin-left: 10px;
+    position: absolute;
+    background-color: rgba(255, 255, 255, 0.719);
+    border-radius: 10px;
+  }
   #id-hashtag {
     font: 150% $sub-title-font;
   }
