2b8fcf2a04893d9a516a51bbfb59732d42a275cd
daklo91 <dennis.bjorneset@gmail.com>
2020-12-10 10:31:47 +0100

    Add search by ID

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,90 @@
 <template>
-  <div>
-    <p>Search</p>
+  <div class="flex-container">
+    <div class="spacer"></div>
+    <img
+      v-if="!confirmedInputSearch"
+      class="img"
+      src="../assets/PikachuByDoopliss.jpg"
+      alt="Pikachu"
+    />
+    <PokemonCard :pokemonArray="pokemonSearchArray" />
+    <p>Index or Name</p>
+    <div>
+      <button
+        style="background-color: red"
+        @click.prevent="clearInput"
+        class="button"
+      >
+        Clear
+      </button>
+      <input
+        @keyup.enter="searchByID"
+        class="input"
+        type="text"
+        v-model="inputSearch"
+      />
+      <button
+        style="background-color: limegreen"
+        @click.prevent="searchByID"
+        class="button"
+      >
+        Search
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import PokemonCard from '@/components/PokemonCard.vue'
+export default {
+  components: {
+    PokemonCard
+  },
+  computed: {
+    pokemonSearchArray() {
+      if (this.confirmedInputSearch) {
+        return [this.$store.state.pokemonArray[this.confirmedInputSearch - 1]]
+      } else return []
+    }
+  },
+  data() {
+    return {
+      inputSearch: null,
+      confirmedInputSearch: null,
+      pokemonByID: []
+    }
+  },
+  methods: {
+    searchByID() {
+      this.confirmedInputSearch = this.inputSearch
+      this.inputSearch = null
+    },
+    clearInput() {
+      this.inputSearch = null
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.spacer {
+  padding: 50px;
+}
+
+.img {
+  width: 450px;
+}
+
+.input {
+  margin-top: 20px;
+  width: 200px;
+  height: 30px;
+  border-radius: 8px;
+}
+</style>
