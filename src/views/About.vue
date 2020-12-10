f5b0db0551618b9b97cab7d763607edbf53f4a23
daklo91 <dennis.bjorneset@gmail.com>
2020-12-10 10:32:11 +0100

    Add art by Doopliss

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,11 +5,24 @@
       api from PokeApi:
     </p>
     <p><a href="https://pokeapi.co/">https://pokeapi.co/</a></p>
+    <p>
+      Git:
+      <a href="https://github.com/PokeAPI/pokeapi"
+        >https://github.com/PokeAPI/pokeapi</a
+      >
+    </p>
     <br />
-    <p>svg from duiker101:</p>
+    <p>svg by duiker101 @ github:</p>
     <p>
       <a href="https://github.com/duiker101/pokemon-type-svg-icons"
         >https://github.com/duiker101/pokemon-type-svg-icons</a
+      >
+    </p>
+    <br />
+    <p>
+      Detective Pikachu art by Doopliss @ zeroChan
+      <a href="https://www.zerochan.net/2557713"
+        >https://www.zerochan.net/2557713</a
       >
     </p>
   </div>
