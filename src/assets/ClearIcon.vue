6a496428584aa72971295830e14922b12017107e
daklo91 <dennis.bjorneset@gmail.com>
2020-12-14 15:41:30 +0100

    Add SVG Icons for Search Comp

---
+++ src/assets/ClearIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 142.42 142.42"
+    height="30"
+    width="30"
+  >
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="iconDenied">
+        <path
+          class="cls-1"
+          d="M71.21,15.5A55.71,55.71,0,1,1,15.5,71.21,55.77,55.77,0,0,1,71.21,15.5m0-15a70.71,70.71,0,1,0,70.71,70.71A70.71,70.71,0,0,0,71.21.5Z"
+        />
+        <polygon
+          class="cls-2"
+          points="107.62 45.95 96.61 34.81 72.23 59.5 49.25 36.52 38.64 47.13 61.69 70.17 37.38 94.79 48.39 105.93 72.77 81.24 95.75 104.22 106.36 93.61 83.31 70.57 107.62 45.95"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.cls-1 {
+  fill: salmon;
+}
+.cls-1,
+.cls-2 {
+  stroke: #000;
+  stroke-miterlimit: 10;
+}
+.cls-2 {
+  fill: salmon;
+}
+</style>
